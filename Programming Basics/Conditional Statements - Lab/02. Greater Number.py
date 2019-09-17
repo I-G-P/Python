200c8148bfee0e72bc38feb6591e4c2d93e1489e
I-G-P <ivangpopow@gmail.com>
2019-09-17 20:24:40 +0300

    Add exercise

---
+++ Programming Basics/Conditional Statements - Lab/02. Greater Number.py
@@ -0,0 +1,6 @@
+first_number = int(input())
+second_num = int(input())
+if first_number>second_num:
+    print(first_number)
+else:
+    print(second_num)
---
+++ Programming Basics/Conditional Statements - Lab/06. Password Guess.py
@@ -0,0 +1,6 @@
+pasword = input()
+kee = "s3cr3t!P@ssw0rd"
+if pasword == kee:
+    print("Welcome")
+else:
+    print("Wrong password!")
